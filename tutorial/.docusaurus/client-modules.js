1f4a79c03bed2e5fd9a5aa5ce51030514ec37fb6
Eduardo dos Santos Leal <engenheiroeduardodossantosleal@gmail.com>
2024-06-11 07:26:16 -0300

    Update

--- tutorial/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/eduardosantosleal/src/github.com/tcarvi-engenheiro-eduardo-leal/python/tutorial/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/eduardosantosleal/src/github.com/tcarvi-engenheiro-eduardo-leal/python/tutorial/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/eduardosantosleal/src/github.com/tcarvi-engenheiro-eduardo-leal/python/tutorial/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/eduardosantosleal/src/github.com/tcarvi-engenheiro-eduardo-leal/python/tutorial/src/css/custom.css"),
-];
